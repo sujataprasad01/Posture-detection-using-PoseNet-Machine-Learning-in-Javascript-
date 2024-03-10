81b5486f0487bb0ef8303b92b7561231720054c1
sujataprasad01 <sujataprasad2004@gmail.com>
2024-03-10 19:40:48 +0530

    done

--- sketch.js
+++ sketch.js
@@ -1,47 +1,58 @@
 let shahrukh_img;
 let capture;
-
+let posenet;
+let noseX, noseY;
+let reyeX, reyeY;
+let leyeX, leyeY;
+let singlePose, skeleton;
+let actor_img;
+let specs, smoke;
 function setup(){
     createCanvas(800, 500);
-    shahrukh_img=loadImage('images/shahrukh.png');
+    // shahrukh_img=loadImage('images/shahrukh.png');
     capture=createCapture(VIDEO)
     capture.hide();
+
+    posenet=ml5.poseNet(capture, modelLoaded);
+    posenet.on('pose', receivedPoses)
+
+    actor_img=loadImage('images/shahrukh.png')
+    specs=loadImage('images/spects.png')
+    smoke=loadImage('images/cigar.png')
 }
 
-function getRandomArbitrary(min, max){
-    return Math.random() *(max-min)+min;
+function receivedPoses(poses){
+    console.log(poses);
+
+    if(poses.length>0){
+        singlePose=poses[0].pose;
+        skeleton=poses[0].skeleton;
+    }
+}
+
+function modelLoaded(){
+      console.log('Model has loaded');
 }
 
 function draw(){
-    // background(200);
 
-    // point(200,200);
+    image(capture, 0, 0, 640, 500);
 
-    // line(200,200,300,300);
+    fill(255,0,0);
 
-    // triangle(100,200,300,400,150,450);
+    if(singlePose){
+        for(let i=0; i<singlePose.keypoints.length; i++){
+            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y, 20)
+        }
+        stroke(255,255,255);
+        strokeWeight(4)
+        for(let j=0; j<skeleton.length; j++){
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
+        }
+        // image(actor_img, singlePose.nose.x-45, singlePose.nose.y-55, 100, 100)
+        image(specs, singlePose.nose.x-45, singlePose.nose.y-55, 100, 100)
+        image(smoke, singlePose.nose.x-45, singlePose.nose.y+45, 40, 40)
+    }
 
-    // rect(500,200, 100,100);
-
-    // // stroke(255,0,0,255);
-    // // strockWeight(5);
-    // fill(132,100,34,200);
-    // ellipse(600,300,100,100);
-
-
-
-    // r=getRandomArbitrary(0,255);
-    // g=getRandomArbitrary(0,255);
-    // b=getRandomArbitrary(0,255);
-
-    // fill(r,g,b);
-    // ellipse(mouseX, mouseY, 50,50);
-
-
-
-    // image(shahrukh_img, mouseX, mouseY, 100, 100)
-
-
-    image(capture, 0, 0, 800, 600);
 
 }
